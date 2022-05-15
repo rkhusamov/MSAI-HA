dd7d00daced421fc3ce840d8fd6c791779dc581f
rkhusamov <khusamov@phystech.edu>
2022-05-15 16:41:36 +0300

    comm2

--- HA6/ha6.py
+++ HA6/ha6.py
@@ -2,11 +2,24 @@
 # 1. Get vending machines content difference/union/intersection (A - B = {'chips': 3, 'water: 5, ...}, amount difference should be positive)
 # 2. Compare vending machines by the amount of cash inside (A > B = True)
 
+class Goods:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class Liquid(Goods):
+    pass
+
+
+class Snack(Goods):
+    pass
+
+
 class VendingMachine:
     def __init__(self, items, cash):
         # which items are presented on which shelves
         self._items = items
-        # TODO here add different types of items (e.g. liquids, snacks, ...)
         # how much money inside machine to give change
         self._cash = cash
         # which purchases users made
@@ -17,38 +30,39 @@ class VendingMachine:
         return self._items
 
     def get_cash_amount(self):
-        return sum([k*v for k, v in self._cash.items()])
+        return sum([k * v for k, v in self._cash.items()])
 
-    def __str__(self):
-        return f'Items: {self._items} | Cash: {self._cash} | Total: {self.get_cash_amount()}'
+    # def __str__(self):
+    #     return f'Items: {self._items} | Cash: {self._cash} | Total: {self.get_cash_amount()}'
+    #
+    # def __gt__(self, other):
+    #     return self.get_cash_amount() > other.get_cash_amount()
+    #
+    # def __sub__(self, other):
+    #     union_keys = set(self._items.keys()).union(other.items.keys())
+    #     diff = {}
+    #     for k in union_keys:
+    #         diff[k] = max(0, self._items.get(k, 0) - other.items.get(k, 0))
+    #     return diff
+    #
+    # def __or__(self, other):
+    #     union_keys = set(self._items.keys()).union(other.items.keys())
+    #     union = {}
+    #     for k in union_keys:
+    #         union[k] = self._items.get(k, 0) + other.items.get(k, 0)
+    #     return union
+    #     return self._items
+    #
+    # def __and__(self, other):
+    #     union_keys = set(self._items.keys()).union(other.items.keys())
+    #     intersection = {}
+    #     for k in union_keys:
+    #         intersection_key = min(self._items.get(k, 0), other.items.get(k, 0))
+    #         if intersection_key > 0:
+    #             intersection[k] = intersection_key
+    #     return intersection
+    #     return self._items
 
-    def __gt__(self, other):
-        return self.get_cash_amount() > other.get_cash_amount()
-
-    def __sub__(self, other):
-        union_keys = set(self._items.keys()).union(other.items.keys())
-        diff = {}
-        for k in union_keys:
-            diff[k] = max(0, self._items.get(k, 0) - other.items.get(k, 0))
-        return diff
-
-    def __or__(self, other):
-        union_keys = set(self._items.keys()).union(other.items.keys())
-        union = {}
-        for k in union_keys:
-            union[k] = self._items.get(k, 0) + other.items.get(k, 0)
-        return union
-        return self._items
-
-    def __and__(self, other):
-        union_keys = set(self._items.keys()).union(other.items.keys())
-        intersection = {}
-        for k in union_keys:
-            intersection_key = min(self._items.get(k, 0), other.items.get(k, 0))
-            if intersection_key > 0:
-                intersection[k] = intersection_key
-        return intersection
-        return self._items
 
 items_a = {'Alenka': 2, 'RitterSport': 4, 'Snickers': 4}
 cash_left_a = {10: 100, 5: 100, 1: 99}
